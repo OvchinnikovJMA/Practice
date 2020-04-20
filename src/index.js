b6dae1be2e2c4340b3f6db927b3e39eb9471ed19
OvchinnikovJMA <DegraDanko@gmail.com>
2020-04-21 00:19:05 +0500

    good

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Post from './Post'
+import './styles/styles.css'
+import './styles/styles.scss'
+import second from './assets/2.png'
+import compact from './assets/compact.png'
+import footerIm from './assets/footerIm.png'
+import logo from './assets/logo.png'
+import thone_anip2 from './assets/thone_anip2.png'
+import top_photo from './assets/top_photo.png'
+const post = new Post('Webpack Post Title')
+
